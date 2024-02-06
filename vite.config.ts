625b728826cc5364fda6e86bd3fd01d1a6457801
Lewis Hunt <lewhunt@live.com>
2024-02-06 12:05:15 +0000

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-tv-player/",
+  //base: "/react-tv-player/", // when deploying on github
   plugins: [react()],
   test: {
     globals: true,
